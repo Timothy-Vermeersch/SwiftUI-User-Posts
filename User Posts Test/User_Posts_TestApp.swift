74cce13350ca22fd5b346c7b21ec2b00e1895cb0
timothyVermUCSC <timothy.d.vermeersch@gmail.com>
2023-06-22 16:11:28 -0700

    Initial Commit

---
+++ User Posts Test/User_Posts_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  User_Posts_TestApp.swift
+//  User Posts Test
+//
+//  Created by Timothy Vermeersch on 6/22/23.
+//
+
+import SwiftUI
+
+@main
+struct User_Posts_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
